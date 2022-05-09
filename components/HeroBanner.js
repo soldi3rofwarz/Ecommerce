9d069c62ab301a69c15527c98e0f38f4aa985027
soldi3rofwarz <josejehovanicastroperez@gmail.com>
2022-05-09 17:45:08 -0600

    agregando la conexion a sanity

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import Link from 'next/link'
+import {urlFor} from '../lib/client'
 
-const HeroBanner = () => {
+const HeroBanner = ({HeroBanner}) => {
   return (
-    <div>HeroBanner</div>
+    <div className='hero-banner-container'>
+      <div>
+        <p className='beats-solo'>{HeroBanner.smallText}</p>
+        <h3>{HeroBanner.midText}</h3>
+        <h1>{HeroBanner.largeText1}</h1>
+        <img src={urlFor(HeroBanner.image)} alt='headphones' className='hero-banner-image'/>
+        <div>
+          <Link href={`/product/${HeroBanner.product}`}>
+            <button type='button'>{HeroBanner.buttonText}</button>
+          </Link> 
+          <div className='desc'>
+            <h5>description</h5>
+            <p>{HeroBanner.desc}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,34 @@
 import React from 'react'
+import {client} from '../lib/client'
 import {Cart,Footer,HeroBanner,Product,Navbar,Layout,FooterBanner} from './../components/index'
 
-const Home = () => {
+const Home = ({products, bannerData}) => {
   return (
     <>
-    HeroBanner
-
+    <HeroBanner HeroBanner={bannerData.length && bannerData[0]}/>
+    {console.log("data",bannerData)}
     <div className='products-heading'>
       <h2>best selling Product</h2>
       <p>Speakers of many variations</p>
     </div>
 
     <div className='products-container'>
-      {['product1 ','product2 '].map((product)=>product)
+      {products?.map((product)=>product.name)
       
       }
     </div>
+    <FooterBanner/>
     </>
   )
 }
+export const getServerSideProps = async ()=>{
+  const query ='*[_type=="Product"]'
+  const products =  await client.fetch(query)
 
+  const bannerquery ='*[_type=="banner"]'
+  const bannerData =  await client.fetch(bannerquery)
+  return {
+    props:{products, bannerData}
+  }
+}
 export default Home
