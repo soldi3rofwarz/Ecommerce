623c10520ee3d22e7a17dff4ba384e89fe16076f
soldi3rofwarz <josejehovanicastroperez@gmail.com>
2022-05-10 17:15:03 -0600

    Agregando Layout

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,11 @@ const Home = ({products, bannerData}) => {
     </div>
 
     <div className='products-container'>
-      {products?.map((product)=>product.name)
+      {products?.map((product)=><Product key={product._id} product={product}/>)
       
       }
     </div>
-    <FooterBanner/>
+    <FooterBanner FooterBanner={bannerData&& bannerData[0]}/>
     </>
   )
 }
